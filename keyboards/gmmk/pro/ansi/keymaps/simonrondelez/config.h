db9aaa402f9a268f4f452f868aeaf2dcd8184203
Simon Rondelez <simonrondelez@me.com>
2021-12-11 10:31:13 +0100

    add personal config

--- keyboards/gmmk/pro/ansi/keymaps/simonrondelez/config.h
+++ keyboards/gmmk/pro/ansi/keymaps/simonrondelez/config.h
@@ -1,3 +1,8 @@
 #pragma once
 
-#define RGB_DISABLE_WHEN_USB_SUSPENDED true
+#define RGBLIGHT_DEFAULT_MODE SOLID_WHITE
+
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
+#define RGB_DISABLE_TIMEOUT 300000 // number of milliseconds (5 minutes) to wait until rgb automatically turns off
+
+#define RGB_MATRIX_KEYPRESSES
